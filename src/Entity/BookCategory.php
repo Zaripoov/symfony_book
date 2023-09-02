bb7efaaa34764d0e329c4bdd6d578b19ea74faea
Zaripov <ruzal.zarip1324@gmail.com>
2023-09-02 14:28:37 +0300

    test

--- src/Entity/BookCategory.php
+++ src/Entity/BookCategory.php
@@ -19,6 +19,13 @@ class BookCategory
     #[ORM\Column(length: 255)]
     private string $slug;
 
+    public function setId(?int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
