21d8fe18f13cc597bf6042b6bcecd0c82d60d147
Zaripov <ruzal.zarip1324@gmail.com>
2023-09-05 23:38:21 +0300

    test

---
+++ tests/AbstractRepositoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+abstract class AbstractRepositoryTest extends KernelTestCase
+{
+    protected ?EntityManagerInterface $em ;
+
+    protected function setUp(): void
+    {
+        $this->em = self::getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    protected function getRepositoryForEntity(string $entityClass): mixed
+    {
+        return $this->em->getRepository($entityClass);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->em->clouse();
+        $this->em = null;
+    }
+}
